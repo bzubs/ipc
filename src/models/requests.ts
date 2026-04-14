cf8b2f1f4fd2c9a0d9cbffdaa8c7125f2642eb57
bzubs <bzubs011@gmail.com>
2026-04-14 19:24:54 +0530

    first commit: data models for incoming request

---
+++ src/models/requests.ts
@@ -0,0 +1,15 @@
+export interface Request {
+    requestType : string,
+    topic : string
+    message : string
+}
+
+
+// export interface pubRequest extends Request {
+//     requestType : "PUBLISH"
+// }
+
+// export interface subRequest extends Request {
+//     requestType : "SUBSCRIBE"
+// }
+
