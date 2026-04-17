7842edb17db1fc71b52eeeb6ade8267a1d6ade11
bzubs <bzubs011@gmail.com>
2026-04-18 01:00:43 +0530

    added http Server for topic management

---
+++ src/http/server.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import type { postTopicReq } from "../models/httpReq.js";
+import { globalQueue, topicExists } from "../store.js";
+import type { event, Subscriber } from "../store.js";
+
+
+export function startHTTPServer(){
+
+    const app = express()
+
+    app.use(express.json())
+
+
+    app.get('/', (req, res) => {
+        res.status(200).send({ msg: "nice" });
+
+    })
+
+    app.post('/topics', (req, res) => {
+        const body: postTopicReq = req.body;
+
+        const topic = body.topic;
+
+        if (!topic) {
+            return res.status(400).json({ status: "error", msg: "topic required" });
+        }
+
+        let valid, _ = topicExists(topic);
+
+        if(valid){
+            res.status(403).json({status : "error", msg : "topic already exists"});
+        }
+        else{
+            globalQueue.push({ topic: topic, queue: Array<event>(), subs: Array<Subscriber>() });
+            res.status(201).json({status : "ok", msg : "topic created now"});                
+        }
+
+    });
+
+    app.get('/topics', (req, res)=>{
+        let data = "";
+        globalQueue.forEach((e)=>{
+            data += e.topic;
+            data += "\t";
+        })
+
+        res.status(200).json({status : "ok", topics : data});
+    });
+
+
+
+
+    app.listen(3000, () => {
+        console.log("Express Server running on port 3000");
+    });
+
+}
