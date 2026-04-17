34c0344daf578f5c91be56fed44d02a65a40c024
bzubs <bzubs011@gmail.com>
2026-04-17 19:48:15 +0530

    added ack; message replay for reconnect

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,31 @@
 import { WebSocketServer, WebSocket } from "ws"
-import type {Request} from "./models/requests.js";
+import type { Request, pubRequest, subRequest, ackRequest } from "./models/requests.js";
 
-
+let count: number = 0;
 
 type event = {
-    status : string
-    message : string
+    id: number,
+    status: string
+    message: string
 
 }
 
 type Subscriber = {
-    socket : WebSocket,
-    topic : string
+    consumerId: string
+    socket: WebSocket,
+    topic: string
+    lastEventId: number
 }
 
 
 type topicQueue = {
     topic: string,
-    queue : Array<event>
-    subs : Array<Subscriber>
+    queue: Array<event>
+    subs: Array<Subscriber>
 }
 
 
-let globalQueue : Array<topicQueue> = new Array<topicQueue>();
+let globalQueue: Array<topicQueue> = new Array<topicQueue>();
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -40,87 +43,135 @@ function topicExists(topic: string) {
 wss.on('connection', (socket) => {
 
     socket.on('message', (msg) => {
-        try {
+        const data = msg.toString();
 
-            const data = msg.toString();
+        // console.log("Raw:", data);
 
-            // console.log("Raw:", data);
+        const parsed: Request = JSON.parse(data);
+        console.log("Parsed:", parsed);
 
-            const parsed: Request = JSON.parse(data);
+        if (parsed.requestType === "SUBSCRIBE") {
 
+
+
+            const parsed: subRequest = JSON.parse(data);
             console.log("Parsed:", parsed);
 
-            if (parsed.requestType === "SUBSCRIBE") {
-                console.log("Subscribing to:", parsed.topic);
-
-                let res = topicExists(parsed.topic)
 
 
-                let isTopic: boolean = res.valid;
-                let object = res.object;
+            console.log("Subscribing to:", parsed.topic);
 
-                if(isTopic){
-                    if(object){
-                        let element: topicQueue = object;
-                        element.subs.push({ socket: socket, topic: parsed.topic });
-                        socket.send(JSON.stringify({
-                            status: "ok",
-                            msg: "subsrcibed"
-                        }));
+            let res = topicExists(parsed.topic)
 
-                    }
-                    
 
+            let isTopic: boolean = res.valid;
+            let object = res.object;
+
+            if (isTopic && object) {
+                let element: topicQueue = object;
+                let subscriber = element.subs.find(s => s.consumerId === parsed.consumerId);
+                if (subscriber) {
+                    subscriber.socket = socket;
+                    element.queue.forEach((e) => {
+                        if (e.id > subscriber.lastEventId) {
+                            e.status = "IN-FLIGHT";
+                            subscriber.socket.send(JSON.stringify(e));
+                        }
+                    })
                 }
-                else{
+                else {
+                    let element: topicQueue = object;
+                    element.subs.push({ consumerId: parsed.consumerId, socket: socket, topic: parsed.topic, lastEventId: -1 });
                     socket.send(JSON.stringify({
-                        status : "failed",
-                        msg : "no such topic"
-                    }))
-                }
-                
-
-
-            }
-
-            if(parsed.requestType==="PUBLISH") {
-                
-                
-                    globalQueue.push({topic: parsed.topic, queue : Array<event>(), subs : Array<Subscriber>()})
-                    socket.send(JSON.stringify({
-                        status : "ok",
-                        msg : "topic published now"
+                        status: "ok",
+                        msg: "subsrcibed"
                     }));
 
-                    let eve : event = {status: "init", message: parsed.message}
-                    let element : topicQueue;
-
-                    let res2 = topicExists(parsed.topic)
-
-                    if(res2.valid && res2.object){
-                        element = res2.object;
-                        element.queue.push(eve);
-                        element.subs.forEach((s)=>{
-
-                            element.queue.forEach((e)=>{
-                                s.socket.send(JSON.stringify(e));
-                            })
-
-
+                    let subscriber = element.subs.find(s => s.consumerId === parsed.consumerId);
+                    if (subscriber) {
+                        element.queue.forEach((e) => {
+                            if (e.id > subscriber.lastEventId)
+                                e.status = "IN-FLIGHT";
+                            subscriber.socket.send(JSON.stringify(e));
                         });
-
                     }
-                
+                }
+
+
+
+
+            }
+            else {
+                socket.send(JSON.stringify({
+                    status: "failed",
+                    msg: "no such topic"
+                }))
             }
 
-            
-        } catch (err) {
-            console.error("Invalid message:", err);
 
+
+        }
+
+        if (parsed.requestType === "PUBLISH") {
+
+            const parsed: pubRequest = JSON.parse(data);
+            console.log("Parsed:", parsed);
+
+            globalQueue.push({ topic: parsed.topic, queue: Array<event>(), subs: Array<Subscriber>() })
             socket.send(JSON.stringify({
-                status: "error",
-                message: "Invalid JSON format"
+                status: "ok",
+                msg: "topic published now"
             }));
+
+            let eve: event = { id: count++, status: "PENDING", message: parsed.message }
+            let element: topicQueue;
+
+            let res = topicExists(parsed.topic)
+
+            if (res.valid && res.object) {
+                element = res.object;
+                element.queue.push(eve);
+                element.subs.forEach((s) => {
+
+                    element.queue.forEach((e) => {
+                        if (e.id > s.lastEventId) {
+                            e.status = "IN-FLIGHT"
+                            s.socket.send(JSON.stringify(e));
+
+                        }
+
+                    })
+
+
+                });
+
+            }
+
+        }
+
+        if (parsed.requestType === "ACK") {
+
+            const parsed: ackRequest = JSON.parse(data)
+
+            let res = topicExists(parsed.topic)
+
+            if (res.valid && res.object) {
+
+                let element = res.object;
+
+                const event = element.queue.find(e => e.id === parsed.eventid);
+                const subscriber = element.subs.find(s => s.consumerId === parsed.consumerId)
+
+                if (event) {
+                    event.status = "ACKNOWLEDGED";
+                }
+                if (subscriber) {
+                    subscriber.lastEventId = parsed.eventid
+                }
+
+
+            }
+
         }
 
     })
