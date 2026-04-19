8a888b2e2bd3650849864829644fd7ba97daa618
bzubs <bzubs011@gmail.com>
2026-04-19 18:58:51 +0530

    websocket request models

---
+++ src/models/wsReq.ts
@@ -0,0 +1,25 @@
+export interface Request {
+    requestType : string
+}
+
+
+export interface pubRequest extends Request {
+    requestType : "PUBLISH"
+    topic : string
+    message : string
+}
+
+export interface subRequest extends Request {
+    requestType : "SUBSCRIBE"
+    consumerId : string
+    groupId : string
+    topic : string
+}
+
+export interface ackRequest extends Request {
+  requestType : "ACK-"
+  consumerId : string
+  topic : string
+  eventid : number  
+}
+
